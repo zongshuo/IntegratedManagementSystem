ff73e8dd22e98bc21083004acfe07670ca71da19
zongshuo <1737290510@qq.com>
2021-07-15 18:27:22 +0800

    添加文件

---
+++ system/src/main/java/com/zongshuo/service/OrganizationPersonService.java
@@ -0,0 +1,25 @@
+package com.zongshuo.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.zongshuo.model.OrganizationPersonModel;
+import com.zongshuo.model.UserModel;
+import com.zongshuo.util.PageParam;
+import com.zongshuo.util.PageResult;
+
+public interface OrganizationPersonService extends IService<OrganizationPersonModel> {
+
+    /**
+     * 分页获取组织人员列表
+     * @param pageParam
+     * @return
+     */
+    PageResult<UserModel> getPage(PageParam<OrganizationPersonModel> pageParam);
+
+    /**
+     * 新增组织人员关系
+     * @param orgId
+     * @param userId
+     */
+    void addOrgPerson(Integer orgId, Integer userId);
+
+}
